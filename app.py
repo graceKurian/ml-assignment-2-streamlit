84ad37e6cc911560aa7087c08fb1f8e6ec8317d8
graceKurian <2025aa05412@wilp.bits-pilani.ac.in>
2026-02-09 12:11:58 +0530

    Add initial Streamlit app file

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="ML Assignment 2", layout="wide")
+
+st.title("Machine Learning Assignment 2")
+st.write("Classification Models – Streamlit App")
